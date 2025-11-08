72f7b0639f378ea5654f42c9f80884f34a3a9bc7
Joel Mitchell <joey5533a@gmail.com>
2025-11-08 14:55:19 -0600

    First commit

--- controller/employeeController.js
+++ controller/employeeController.js
@@ -1,3 +1,27 @@
+import Employee from '../model/Employee.js'
+
+export const create = async(req, res) => {
+    try {
+        const { employeeData = new Employee() } = (req.body)
+        const { userName } = emploueeData
+        const existingEmployee = await Employee.findOne({ userName})
+
+        if(employeeExist) {
+            return res.status(400).json({ error: "Employee with this username already exists" })
+        }
+
+        const savedEmployee = await employeeData.save();
+        res.status(200).json(savedEmployee)
+    } catch (error) {
+        res.status(500).json({ error: "Internal Server Error" })
+
+    }
+}
+
+
+
+
+
 export const fetch = async(req, res) => {
     try {
         res.json("Hello World")
--- route/employeeRoute.js
+++ route/employeeRoute.js
@@ -2,6 +2,7 @@ import express from "express"
 import {fetch} from "../controller/employeeController.js"
 const route = express.Router()
 
+route.post("/create", create)
 route.get("/fetch", fetch)
 
 export default route
